d4f79b6f4883462782cddd8a93af849ebed29980
yanagitani <takanori.yanagitani@gmail.com>
2022-08-31 17:31:12 +0900

    feat: batch upserts

--- stdb.go
+++ stdb.go
@@ -10,6 +10,33 @@ import (
 
 type Set func(ctx context.Context, id string, date time.Time, key, val []byte) error
 
+type TsSample struct {
+	id   string
+	date time.Time
+	pair s2k.Pair
+}
+
+func (t TsSample) ToDatesTableName() string               { return "dates_" + t.id }              // dates_cafef00ddeadbeafface864299792458
+func (t TsSample) ToDevicesTableName(dtymd string) string { return "devices_" + dtymd }           // devices_2022_08_31
+func (t TsSample) ToDtDvTableName(dtymd string) string    { return "data_" + dtymd + "_" + t.id } // data_2022_08_31_cafef00ddeadbeafface864299792458
+func (t TsSample) AsKey() []byte                          { return t.pair.Key }
+func (t TsSample) AsVal() []byte                          { return t.pair.Val }
+
+func (t TsSample) ToBatch(d2s Date2Str) s2k.Iter[s2k.Batch] {
+	bid := []byte(t.id)
+	ymd := d2s(t.date)
+	bym := []byte(ymd)
+	return s2k.IterFromArray([]s2k.Batch{
+		s2k.BatchNew("devices", bid, nil),
+		s2k.BatchNew("dates", bym, nil),
+		s2k.BatchNew(t.ToDatesTableName(), bym, nil),
+		s2k.BatchNew(t.ToDevicesTableName(ymd), bid, nil),
+		s2k.BatchNew(t.ToDtDvTableName(ymd), t.AsKey(), t.AsVal()),
+	})
+}
+
+type BatchSet func(ctx context.Context, b s2k.Iter[TsSample]) error
+
 func newStName(id string, ymd string) string {
 	return strings.Join([]string{
 		"data",
@@ -56,6 +83,54 @@ func NewSetter(adder s2k.AddBucket, setter s2k.Set) func(dateConverter Date2Str,
 				cmdRunner.UpsertBuilder(dvdate, setter)(ctx, devid, []byte("")), // devices_20220826
 				cmdRunner.UpsertBuilder(stname, setter)(ctx, key, val),
 			})
+		}
+	}
+}
+
+func fastBucketAdderNew(a s2k.AddBucket) s2k.AddBucket {
+	// TODO memoize
+	return a
+}
+
+func IterFlat2Chan[T any](i s2k.Iter[s2k.Iter[T]], c chan<- T, lmt int) {
+	j := 0
+	for oi := i(); oi.HasValue(); oi = i() {
+		var i s2k.Iter[T] = oi.Value()
+		for o := i(); o.HasValue(); o = i() {
+			var t T = o.Value()
+			if j < lmt {
+				c <- t
+			}
+			j += 1
+		}
+	}
+}
+
+func IterMitm[T any](i s2k.Iter[T], f func(T)) s2k.Iter[T] {
+	return func() s2k.Option[T] {
+		var o s2k.Option[T] = i()
+		if o.HasValue() {
+			f(o.Value())
+		}
+		return o
+	}
+}
+
+func samples2batch(s s2k.Iter[TsSample], d2s Date2Str, lmt int) s2k.Iter[s2k.Batch] {
+	mapd := s2k.IterMap(s, func(ts TsSample) s2k.Iter[s2k.Batch] { return ts.ToBatch(d2s) })
+	c := make(chan s2k.Batch, lmt)
+	IterFlat2Chan(mapd, c, lmt)
+	close(c)
+	return s2k.IterFromChan(c)
+}
+
+func NewBatchSetter(fastAdder s2k.AddBucket, setter s2k.SetBatch, lmt int) func(dateConverter Date2Str, runner CommandRunner) BatchSet {
+	return func(dateConverter Date2Str, cmdRunner CommandRunner) BatchSet {
+		return func(ctx context.Context, many s2k.Iter[TsSample]) error {
+			var bi s2k.Iter[s2k.Batch] = samples2batch(many, dateConverter, lmt)
+			return setter(ctx, IterMitm(bi, func(b s2k.Batch) {
+				_ = fastAdder(ctx, b.Bucket()) // unable to create missing table -> query will be rejected anyway
+			}))
 		}
 	}
 }
